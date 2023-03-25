610cf83617775478389b45a71a4d4dc3e73cbeaa
Joona Nykänen <joona.nykanen@student.lut.fi>
2023-03-25 15:55:22 +0200

    fix: stop-function to use destroy() instead of end()

--- index.js
+++ index.js
@@ -110,12 +110,19 @@ function skip(message, serverQueue) {
 }
 
 function stop(message, serverQueue) {
-  if (!message.member.voice.channel) {
-    return message.channel.send('You have to be in a voice channel to stop the music!');
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could stop!");
+    if (serverQueue.connection && serverQueue.connection.dispatcher) {
+      serverQueue.songs = [];
+      serverQueue.connection.dispatcher.destroy(); // Use destroy() instead of end()
+    }
   }
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
-}
+  
+  
 
 function play(guild, song) {
     const serverQueue = queue.get(guild.id);
